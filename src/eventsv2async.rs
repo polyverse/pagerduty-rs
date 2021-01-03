07b82181b2ce72891adfd7addc3a5a72208513c3
Archis Gore <archis@polyverse.com>
2021-01-03 13:42:22 -0800

    Move tokio out as an explicit dependency, and go through reqwest

--- src/eventsv2async.rs
+++ src/eventsv2async.rs
@@ -1,16 +1,17 @@
 use crate::private_types::*;
 use crate::types::*;
 
+use reqwest;
+use reqwest::header::{
+    HeaderMap, HeaderValue, InvalidHeaderValue, CONTENT_ENCODING, CONTENT_TYPE, USER_AGENT,
+};
 use serde::Serialize;
 use std::convert::From;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult};
-use reqwest;
-use reqwest::header::{HeaderMap, HeaderValue, CONTENT_ENCODING, CONTENT_TYPE, USER_AGENT, InvalidHeaderValue};
 
 const CONTENT_ENCODING_IDENTITY: &str = "identity";
 const CONTENT_TYPE_JSON: &str = "application/json";
-
 
 #[derive(Debug)]
 pub enum EventsV2Error {
@@ -19,7 +20,7 @@ pub enum EventsV2Error {
 
     //https://developer.pagerduty.com/docs/events-api-v2/overview/#api-response-codes--retry-logic
     HttpNotAccepted(u16), // NOT 4xx, 5xx or 200 (we expect 202). Contains HTTP response code.
-    HttpError(u16) // A legit error (4xx or 5xx). Contains HTTP response code.
+    HttpError(u16),       // A legit error (4xx or 5xx). Contains HTTP response code.
 }
 
 impl Error for EventsV2Error {}
@@ -58,10 +59,12 @@ impl EventsV2 {
         integration_key: String,
         user_agent: Option<String>,
     ) -> Result<EventsV2, EventsV2Error> {
-
         let mut headers = HeaderMap::new();
         headers.insert(CONTENT_TYPE, HeaderValue::from_str(CONTENT_TYPE_JSON)?);
-        headers.insert(CONTENT_ENCODING, HeaderValue::from_str(CONTENT_ENCODING_IDENTITY)?);
+        headers.insert(
+            CONTENT_ENCODING,
+            HeaderValue::from_str(CONTENT_ENCODING_IDENTITY)?,
+        );
         if let Some(ua) = user_agent {
             headers.insert(USER_AGENT, HeaderValue::from_str(ua.as_str())?);
         }
@@ -80,7 +83,9 @@ impl EventsV2 {
         match event {
             Event::Change(c) => self.change(c).await,
             Event::AlertTrigger(at) => self.alert_trigger(at).await,
-            Event::AlertAcknowledge(aa) => self.alert_followup(aa.dedup_key, Action::Acknowledge).await,
+            Event::AlertAcknowledge(aa) => {
+                self.alert_followup(aa.dedup_key, Action::Acknowledge).await
+            }
             Event::AlertResolve(ar) => self.alert_followup(ar.dedup_key, Action::Resolve).await,
         }
     }
@@ -91,33 +96,35 @@ impl EventsV2 {
         self.do_post(
             "https://events.pagerduty.com/v2/change/enqueue",
             sendable_change,
-        ).await
+        )
+        .await
     }
 
     async fn alert_trigger<T: Serialize>(&self, alert_trigger: AlertTrigger<T>) -> EventsV2Result {
-        let sendable_alert_trigger = SendableAlertTrigger::from_alert_trigger(alert_trigger, self.integration_key.clone());
+        let sendable_alert_trigger =
+            SendableAlertTrigger::from_alert_trigger(alert_trigger, self.integration_key.clone());
 
         self.do_post(
             "https://events.pagerduty.com/v2/enqueue",
             sendable_alert_trigger,
-        ).await
+        )
+        .await
     }
 
     async fn alert_followup(&self, dedup_key: String, action: Action) -> EventsV2Result {
-        let sendable_alert_followup = SendableAlertFollowup::new(dedup_key, action, self.integration_key.clone());
+        let sendable_alert_followup =
+            SendableAlertFollowup::new(dedup_key, action, self.integration_key.clone());
 
         self.do_post(
             "https://events.pagerduty.com/v2/enqueue",
             sendable_alert_followup,
-        ).await
+        )
+        .await
     }
 
     // Make this part Async in the future
     async fn do_post<T: Serialize>(&self, url: &str, content: T) -> EventsV2Result {
-        let res = self.client.post(url)
-            .json(&content)
-            .send()
-            .await?;
+        let res = self.client.post(url).json(&content).send().await?;
 
         match res.status().as_u16() {
             202 => Ok(()),
